e28ee6fc490082f9c340a6c42f672d6c4a2e4751
Jeremy Symon <jtsymon@gmail.com>
2020-02-14 19:02:42 +1300

    Support specifying year/recent/modified CVE feeds

--- lib/cve_monitor.rb
+++ lib/cve_monitor.rb
@@ -7,7 +7,7 @@ require 'cve_monitor/lazy_hash'
 # you just need to require this one file in your bin file
 
 module CveMonitor
-  def self.parse_node(node)
+  def parse_node(node)
     cpes = []
     node['children']&.each do |child|
       cpes += parse_node(child)
@@ -31,10 +31,35 @@ module CveMonitor
   end
 
   # Array intersection using comparison instead of hash.
-  def self.intersect?(array1, array2)
+  def intersect?(array1, array2)
     array1.each do |e1|
       return true if array2.any? { |e2| e1 == e2 }
     end
     false
+  end
+
+  def check(feed)
+    uri = "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-#{feed}.json.gz"
+    cves = URI.open(uri) do |f|
+      begin
+        gz = Zlib::GzipReader.new(f)
+        JSON.parse(gz.read)
+      ensure
+        gz&.close
+      end
+    end
+    cves['CVE_Items']&.each do |item|
+      cve = item['cve']
+      cve_id = cve.walk('CVE_data_meta', 'ID')
+      cpes = item.walk('configurations', 'nodes')&.flat_map do |node|
+        parse_node(node)
+      end
+      matching = cpes.select do |cpe|
+        @cpes.any? { |e| e == cpe }
+      end
+      next if matching.empty?
+
+      puts "https://nvd.nist.gov/vuln/detail/#{cve_id}", matching, "\n"
+    end
   end
 end
